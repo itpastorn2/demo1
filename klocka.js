8de9db926a99fd39b18e6bb42633c0329d12b1ea
Lars Gunther <gunther@keryx.se>
2017-12-13 13:36:36 +0100

    up2date 2017-12-13

---
+++ klocka.js
@@ -0,0 +1,6 @@
+// Detta är javaScript
+window.onload = function () {
+    console.log("Jag körs nu"); 
+    var para = document.getElementsByTagName("p")[0];
+    para.innerHTML = "Ändrad text";
+}
